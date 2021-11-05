1c10bbd3d999501cab92d94c5127eaaca8115df5
kimin3927 <kimin3927@gmail.com>
2021-11-05 13:41:30 +0900

    travel세팅

---
+++ travel/js/detail.js
@@ -0,0 +1,10 @@
+
+const showDetail = (e) => {
+  const target = $(e.currentTarget);
+  if(!target.hasClass("open")) {
+    target.addClass("open")
+  } else{
+    target.removeClass("open")
+  }
+}
+
