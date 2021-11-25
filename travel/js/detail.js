4bd2cc3ae889b0951dd619930569256c679e6543
DESKTOP-463V9J6\k <kimin3927@gmail.com>
2021-11-25 22:36:36 +0900

    테스트 커밋 시작

--- travel/js/detail.js
+++ travel/js/detail.js
@@ -1,10 +1,89 @@
 
-const showDetail = (e) => {
-  const target = $(e.currentTarget);
-  // if(!target.hasClass("open")) {
-  //   target.addClass("open")
-  // } else{
-  //   target.removeClass("open")
-  // }
+
+const showArrow = (e) => {
+  const target = $($(e.currentTarget).children()[11]);
+  if(target.data("hoverStatus") !== "on") {
+    target.data("hoverStatus", "on")
+    $(target.children()[0]).removeClass("hidden")
+  } else {
+    target.data("hoverStatus", "off");
+    $(target.children()[0]).addClass("hidden")
+  }
 }
+
+
+
+const arrowClickHandle = (e) => {
+  const btnDiv = $(e.currentTarget).parent();
+  const itsRowID = $(btnDiv.parent()).attr("id");
+  if(!$("#detail").data("status") || $("#detail").data("status") == "closed") {
+    $("#detail").data("status", `${itsRowID}`);
+    console.log($("#detail").data("status"))
+    $("#detail").css("width", "30%");
+    console.log($("#detail").css("width"))
+    $("#map").css("zIndex", "-2");
+    makeDetailContents(itsRowID);
+  } else {
+      if(itsRowID !== $("#detail").data("status")){
+        $("#detail").data("status", `${itsRowID}`);
+        $("#detail").css("width", "30%");
+        $("#map").css("zIndex", "-2")
+        makeDetailContents(itsRowID);
+      } else {
+        $("#detail").text("");
+        $("#map").css("zIndex", "2")
+        $("#detail").data("status", "closed");
+        $("#detail").css("width", "0");
+      }
+  }
+}
+
+$("body").click((e) =>{
+  console.log('body click');
+  if($("#detail").data("status") !== "closed"){
+    $("#detail").text("");
+    $("#map").css("zIndex", "2")
+    $("#detail").data("status", "closed");
+    $("#detail").css("width", "0");
+  }
+})
+
+const makeDetailContents = (id) => {
+  $("#detail").text(``);
+  $("#detail").append(`${id}`);
+}
+
+const afkey = `SDRbmynkhVAJ7Y8GyxJDNvxT8dcE32hBE5%2FjwnFFCKxxusxtWuUy6HfdzRO%2BZ5jBlVxI7jsTvIXBRx4fHFPPeg%3D%3D`;
+
+const apiCall3 = () => {
+  const data = $.ajax({
+    url: `http://api.visitkorea.or.kr/openapi/service/rest/DataLabService/metcoRegnVisitrDDList?serviceKey=${afkey}&pageNo=1&numOfRows=10&MobileOS=WIN&MobileApp=AppTest&startYmd=20210513&endYmd=2021513`,
+    type: "GET",
+  });
+  console.log(data);
+  return data;
+}
+
+apiCall3();
+
+const apiCall_naver = (query, display, start) => {
+  const data = $.ajax({
+    headers : {
+      "X-Naver-Client-Id":"Jiz3dO5sz8acihT6BrKQ",
+      "X-Naver-Client-Secret":"SFcjSlbHui"
+    },
+    url: `https://openapi.naver.com/v1/search/local.xml?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=random`,
+    //`https://openapi.naver.com/v1/search/local.json?query=${query}&dispaly=${display}&start=${start}&sort=random`,
+    type: "GET",
+  });
+  console.log(data);
+  return data;
+}
+
+apiCall_naver("서울", 5, 1);
+
+$("#ktable").mouseenter( (e) => {
+
+})
+
 
